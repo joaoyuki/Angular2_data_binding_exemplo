12c836731103bd98544064827c9a4dc267509abb
Joao Felipe <joao.yuki@gmail.com>
2018-08-14 23:35:09 -0300

    Adicionado data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+import { AlertModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'https://www.google.com.br/search?q=java&source=lnms&tbm=isch&sa=X&ved=0ahUKEwim17yAp-vcAhXDiJAKHXHODMgQ_AUICigB&biw=1680&bih=944';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+  nome: string = 'abc';
+
+  pessoa = {
+    nome: 'joao',
+    idade: 30
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoCliado(){
+    alert('Joao');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    //console.log(<HTMLInputElement>(evento.target).value);
+    this.valorAtual = (<HTMLInputElement>(evento.target).value;
+  }
+
+  salvarValor(valor){
+    console.log(valor);
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+}
